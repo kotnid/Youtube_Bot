47e9997b86018ac679e7ce725a112be2aa4209de
kotnid <tkt0506tkt@gmail.com>
2022-01-26 11:18:48 +0800

    Add /start command to main.py
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from django import dispatch
 from telegram.ext import Updater 
 from telegram.ext import CommandHandler , CallbackQueryHandler
 from telegram.ext import MessageHandler , Filters
@@ -8,3 +9,29 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 token = config['TELEGRAM']['ACCESS_TOKEN']
+
+# init bot and setup dispatcher
+updater = Updater(token=token , use_context=False)
+dispatcher = updater.dispatcher
+
+# start command 
+def start(bot , update):
+    message = update.message
+    chat = message['chat']
+    update.message.reply_text(text='Hi ' + str(chat['username']))
+    update.message.reply_text(text='''\
+    The usage of this bot is download video from Youtube
+    Command available : 
+    /start - show this message
+    \
+    ''')
+
+# add handler to dispatcher
+dispatcher.add_handler(CommandHandler('start' , start))
+
+# start running bot
+updater.start_polling()
+updater.idle()
+
+# stop running bot
+updater.stop()
