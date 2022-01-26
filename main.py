a5e204a4ed370424a9a56153d830c7b185ca6b03
kotnid <tkt0506tkt@gmail.com>
2022-01-26 14:14:02 +0800

    Add download video to main.py
    Signed-off-by: kotnid <tkt0506tkt@gmail.com>

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from telegram.ext import CommandHandler , CallbackQueryHandler
 from telegram.ext import MessageHandler , Filters
 
 import configparser
+from os import  rename , system
+from subprocess import check_output
+from pytube import YouTube
+from string import punctuation
 
 # get token from config
 config = configparser.ConfigParser()
@@ -17,17 +21,33 @@ dispatcher = updater.dispatcher
 # start command 
 def start(bot , update):
     message = update.message
+    print(message)
     chat = message['chat']
     update.message.reply_text(text='Hi ' + str(chat['username']))
     update.message.reply_text(text='''\
     The usage of this bot is download video from Youtube
-    Command available : 
+This bot 
+Command available : 
     /start - show this message
     \
     ''')
 
+# download command
+def download(bot , update):
+    text = update.message['text']
+    url = text[10:]
+    yt = YouTube(url) 
+    yt.streams.get_highest_resolution().download()
+    title = yt.title.translate(str.maketrans('', '', punctuation))
+    rename(f"{yt.title}.mp4" ,title+".mp4")
+    system('curl --upload-file  "{}.mp4" https://transfer.sh --globoff > test.txt'.format(title))
+    with open("test.txt", "r") as file:
+        update.message.reply_text(text = file.read())
+    system('rm -rf *.mp4')    
+
 # add handler to dispatcher
 dispatcher.add_handler(CommandHandler('start' , start))
+dispatcher.add_handler(CommandHandler('download' , download))
 
 # start running bot
 updater.start_polling()
